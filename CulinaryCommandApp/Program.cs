49b039a7750cca85bd4e2c0fa1bc29ca7cdb1803
Kevin Tran <kevtran04@gmail.com>
2026-02-06 14:34:19 -0600

    fix: disable HTTPS redirection in Blazor app

--- CulinaryCommandApp/Program.cs
+++ CulinaryCommandApp/Program.cs
@@ -123,7 +123,7 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-    app.UseHttpsRedirection();
+    // app.UseHttpsRedirection();
 }
 
 // Temporarily disable HTTPS redirect for development
