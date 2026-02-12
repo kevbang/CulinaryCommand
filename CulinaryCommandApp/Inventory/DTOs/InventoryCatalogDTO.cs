e79d7e73c6bb2a4ce0f49502ac1eeaf8e9d031f0
Kevin Tran <kevtran04@gmail.com>
2026-02-12 12:00:45 -0600

    feat: add "/inventory-catalog" endpoint
    ### CC-101
    
    - Adding an "Item/Inventory" catalog so a restaurant owner can see the possible items that can be inside of a store. This does not keep track of the actual stock value of each item or ingredient

---
+++ CulinaryCommandApp/Inventory/DTOs/InventoryCatalogDTO.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace CulinaryCommand.Inventory.DTOs
+{
+    public class InventoryCatalogDTO
+    {
+        public int Id { get; set; }
+        public string ItemName { get; set; } = string.Empty;
+        public string SKU { get; set; } = string.Empty;
+        public string Category { get; set; } = string.Empty;
+        public string Unit { get; set; } = "count";
+        public decimal PricePerUnit { get; set; }
+        public string? Notes { get; set; }
+    }
+}
