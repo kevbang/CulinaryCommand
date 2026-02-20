8bc42cdff37d4e9bc544152e9dd54a04a771db4a
Wyatt Hunter <wyatthunter2102@gmail.com>
2026-02-20 12:32:09 -0600

    Refactor user management and invite flow

--- CulinaryCommandApp/Models/SignupRequest.cs
+++ CulinaryCommandApp/Models/SignupRequest.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using CulinaryCommand.Services;
 
 namespace CulinaryCommand.Models
 {
@@ -62,7 +63,12 @@ namespace CulinaryCommand.Models
         [Required, EmailAddress]
         public string Email { get; set; } = string.Empty;
 
-        [Required]
-        public string Password { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Password is required.")]
+        [CognitoPassword]
+        public string Password { get; set; } = "";
+
+        [Required(ErrorMessage = "Please confirm your password.")]
+        [Compare(nameof(Password), ErrorMessage = "Passwords do not match.")]
+        public string ConfirmPassword { get; set; } = "";
     }
 }
